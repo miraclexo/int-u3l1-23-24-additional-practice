64e65115dc30bba4d12ad3c63c41430a6bba1da0
CN-Kayla <113617344+CN-Kayla@users.noreply.github.com>
2023-12-13 18:48:18 +0000

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,45 @@
+# Code Nation INT Unit 3 Lesson 1: Variables and Data Types (Extra Practice)
+
+This is an extension of the coding practice for Unit 3 Lesson 1, found here: https://github.com/itscodenation/int-u3l1-23-24-student-exercises 
+
+Come up with a MadLib/story about what happened last class (Disclaimer: Events described might not have actually happened). Instructions can be found in the script.js file. There are additional questions at the end of the script.js file. More information on variables and data types can be found below. 
+
+____________________________________________________________________
+
+# Lesson 3.1: Variables & Data Types
+
+## Objective
+Learn about variables in JavaScript, how to declare them, assign values, and manipulate their content.
+
+
+## What is a Variable?
+- A variable acts like a box or container storing a piece of information known as its value.
+- Example: 
+    ```javascript
+    let box;
+    ```
+
+## Naming a Variable:
+- Variable names should be descriptive and relevant.
+- They follow the camelCase convention (e.g., `userName`, `totalAmount`).
+- Restrictions: 
+    - Cannot start with a number.
+    - Cannot be reserved keywords (like `let`).
+
+## Assigning a Value:
+- After declaring a variable, we can assign a value to it.
+- Example: 
+    ```javascript
+    let box;
+    box = 8;
+    ```
+
+## Data Types:
+- **Numbers**: Numerical values, can be positive, negative, or decimals.
+- **Strings**: Series of characters enclosed within quotation marks (either single `' '` or double `" "`).
+
+## Operators:
+- Used to perform arithmetic operations on variables.
+- Basic ones include: `+`, `-`, `*`, `/`.
+
+---
---
+++ script.js
@@ -0,0 +1,64 @@
+// 1. Create a variable named `place` and set the value to a string that says whatever place you want.
+
+// 2. Create a variable named `creatureName` and set the value to a string that says whatever name you want to give the creature.
+
+// 3. Create a variable named `color` and set the value to a string that says whatever color you want.
+
+// 4. Create a variable named `color2` and set the value to a string that says a different color you want.
+
+// 5. Create a variable named `emotion` and set the value to a string that says whatever emotion you want.
+
+// 6. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
+
+// 7. Create a variable named `number` and set the value to a number greater than 1.
+
+// 8. Create a variable named `item` and set the value to a string that says whatever item/thing you want.
+
+// 9. Create a variable named `item2` and set the value to a string that says a different item/thing you want.
+
+// 10. Create a variable named `verb` and set the value to a string that says whatever verb you want.
+
+// 11. Create a variable named `adverb` and set the value to a string that says whatever adverb you want.
+
+// 12. Create a variable named `number2` and set the value to a number greater than 1.
+
+// 13. Replace the null values with the appropriate variable you created above.
+// - Afterwards, run your code and press the button to see your story!
+
+document.querySelector("button").onclick = function() {
+    //replace null below with variable for first volunteer
+     document.querySelector(".volunteer1").innerHTML = null;
+      //replace null below with variable for second volunteer
+ document.querySelector(".volunteer2").innerHTML = null;
+      //replace null below with variable for first number
+    document.querySelector(".number1").innerHTML = null;
+    //replace null below with variable for second number
+    document.querySelector(".number2").innerHTML = null;
+    //replace null below with variable adding numbers
+    document.querySelector(".adding").innerHTML = null;
+     //replace null below with variable subtracting numbers
+    document.querySelector(".subtracting").innerHTML =null;
+  //replace null below with variable for the adjective
+    document.querySelector(".adjective").innerHTML = null;
+     //replace null below with variable for fist student's name
+    document.querySelector(".classmate1").innerHTML = null;
+     //replace null below with variable for second student's name
+    document.querySelector(".classmate2").innerHTML = null;
+     //replace null below with variable for celebrity's name
+    document.querySelector(".celebrity").innerHTML = null;
+    //replace null below with variable for celebrity's name
+     document.querySelector(".celebrity-again").innerHTML=null;
+    //replace null below with variable for the verb
+    document.querySelector(".verb").innerHTML = null;
+     document.querySelector(".storyOne").style="display:block";
+};
+
+// ## (If time): Spicy Thought Provoking Questions:
+
+// 1. Take a look at the HTML (scroll down!) How do you think you were able to make the JavaScript interact with the HTML?
+
+// 2. What would happen if you have put the number values you assigned in quotes? (Try it out and see!)
+
+// 3. What do you think `document.querySelector()` is doing? (Hint: look at what's in the parenthesis following it).
+
+// 4. What do you think .innerHTML is doing?
